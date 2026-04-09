a6c575ec176922ab01e3503727805e0315a4b139
Yen-Chin, Lee <coldnew.tw@gmail.com>
2026-04-09 17:01:43 +0800

    ci: Conditionally find and include GTest in CMake

--- cmake/gtest.cmake
+++ cmake/gtest.cmake
@@ -3,24 +3,27 @@
 
 INCLUDE(FetchContent)
 
-SET(GTEST_VERSION 1.14.0)
+FIND_PACKAGE(GTest QUIET)
+IF(NOT GTest_FOUND)
+  SET(GTEST_VERSION 1.14.0)
 
-FETCHCONTENT_DECLARE(
-  gtest
-  GIT_REPOSITORY https://github.com/google/googletest.git
-  GIT_TAG v${GTEST_VERSION}
-  SOURCE_DIR ${CMAKE_BINARY_DIR}/_deps/gtest-src
-  BINARY_DIR ${CMAKE_BINARY_DIR}/_deps/gtest-build
-)
+  FETCHCONTENT_DECLARE(
+    gtest
+    GIT_REPOSITORY https://github.com/google/googletest.git
+    GIT_TAG v${GTEST_VERSION}
+    SOURCE_DIR ${CMAKE_BINARY_DIR}/_deps/gtest-src
+    BINARY_DIR ${CMAKE_BINARY_DIR}/_deps/gtest-build
+  )
 
-SET(gtest_force_shared_cxxrt ON CACHE BOOL "" FORCE)
-SET(googletest cmake)
-SET(BUILD_GMOCK OFF)
-SET(INSTALL_GTEST OFF)
+  SET(gtest_force_shared_cxxrt ON CACHE BOOL "" FORCE)
+  SET(googletest cmake)
+  SET(BUILD_GMOCK OFF)
+  SET(INSTALL_GTEST OFF)
 
-FETCHCONTENT_MAKEAVAILABLE(gtest)
+  FETCHCONTENT_MAKEAVAILABLE(gtest)
 
-INCLUDE(GoogleTest)
+  INCLUDE(GoogleTest)
+ENDIF()
 
 ADD_FEATURE_INFO(GTest TRUE "Google Test framework")
 
